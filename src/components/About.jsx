86a014b23b860298f394cf416ceb675d762a8939
Pushpesh <ppant9418@gmail.com>
2025-03-28 22:33:54 +0530

    Modified about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <div className="container">
         <div className="about-content bg-zinc-800/50 p-7 rounded-2xl md:p-12">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl font-preahvihear">
-            I'm Pushpesh, a passionate full stack developer who thrives on creating intuitive and innovative digital experiences. With a deep love for both design and code, I enjoy transforming complex challenges into elegant solutions that power engaging user interactions. Whether I'm developing robust back-end systems or crafting visually stunning front-end interfaces, I believe in continuous learning, collaboration, and a commitment to excellence in every project.
+          Hi, I'm Pushpesh, a full-stack developer who enjoys building intuitive and efficient digital experiences. I love solving challenges through code, whether it's designing user-friendly interfaces or developing scalable back-end systems. I'm always eager to learn, collaborate, and refine my skills to create meaningful and impactful projects.
           </p>
         </div>
       </div>
